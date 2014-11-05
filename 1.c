df455c3236f3b7782be669bd7e7d5093a7910382
minonastya <minovskayanastya@gmail.com>
2014-11-05 22:51:50 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,19 +1,21 @@
 //двоичное представление числа
+//Миновская Настя
 #include <stdio.h>
 int main(void)
 {
 int n, i;
 int r = 31;
 scanf("%d", &n);
-if (n==0) printf("%d", 0);
-else{
-for(i=0; ((n>>r) & 1) == 0; i++) n = n<<1;
-for(; i<=r; i++) 
-{
-  printf("%d", (n>>r)&1);
-  n = n<<1; 
-}
-}
+if (n == 0) printf("%d", 0);
+else
+  {
+  for(i=0; ((n>>r) & 1) == 0; i++) n = n<<1;
+  for(; i <= r; i++) 
+    {
+    printf("%d", (n >> r) & 1);
+    n = n << 1; 
+    }
+  }
 printf("\n");
 return 0;
 }
