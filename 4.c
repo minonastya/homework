332102f62800a5003bf1b46958c5ab76c8ff358f
minonastya <minovskayanastya@gmail.com>
2014-11-11 21:11:45 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 int main(void)
 {
-int n,i,k;
-scanf("%d",&n);
-k=(n>>31)&1;
-printf("%d\n",k|(n&1)|(0-k));
+int n, i, k;
+scanf("%d", &n);
+k = (n >> 31) & 1;
+printf("%d\n",k | (n & 1) | (0 - k));
 return 0;
 }
