5e0d5ef718a04678583eb189ca5dafa5da67a971
minonastya <minovskayanastya@gmail.com>
2014-11-26 00:22:20 +0300

    Update float2.c

--- float2.c
+++ float2.c
@@ -3,42 +3,45 @@
 #include <stdio.h>
 int main(void)
 {
-float a, b;
-int p, s, e, m;
-struct{
-int type;
-union{
-int ival;
-float fval;
-} val;
-} data;
+  float a, b;
+  int p, s, e, m;
+  struct
+  {
+    int type;
+    union
+    {
+      int ival;
+      float fval;
+    } val;
+  } data;
 
-scanf("%f%f", &a, %b);
-data.val.fval = a / b;
-p = data.val.ival;
-s = (p >> 31) & 1;
-e = (p >> 23) & ((1 << 8) - 1);
-m = p & ((1 << 23) - 1);
-if (s == 0) 
-{
-	s = 1;
-}
-else s = -1;
-if ((e == 255) && (m == 0) && (s == -1)) 
-{
-	printf("-Infinity\n");
-}
-else if ((e == 255) && (m == 0) && (s == 1)) 
-{
-	printf("+Infinity\n");
-}
-else if ((e == 255) && (m != 0)) 
-{
-	printf("NaN\n");
-}
-else if ((e == 0) && (m == 0)){
-	printf("NULL\n");
-}
-else printf("%d*2^%d*%f\n", s, e-127,1 + ((float)m / (1 << 23)));
-return 0;
+  scanf("%f%f", &a, %b);
+  data.val.fval = a / b;
+  p = data.val.ival;
+  s = (p >> 31) & 1;
+  e = (p >> 23) & ((1 << 8) - 1);
+  m = p & ((1 << 23) - 1);
+  if (s == 0)  
+  {
+    s = 1;
+  }
+  else s = -1;
+  if ((e == 255) && (m == 0) && (s == -1)) 
+  {
+    printf("-Infinity\n"); 
+  }
+  else if ((e == 255) && (m == 0) && (s == 1)) 
+  {
+    printf("+Infinity\n");
+  }
+  else if ((e == 255) && (m != 0)) 
+  {
+    printf("NaN\n");
+  }
+  else if ((e == 0) && (m == 0))
+  {
+    printf("Zero\n");
+  }
+  else printf("%d*2^%d*%f\n", s, e-127,1 + ((float)m / (1 << 23)));
+  return 0;
 }
