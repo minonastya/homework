2c66ce2c1f4e480c7ddf8a3231c0c20299b50272
minonastya <minovskayanastya@gmail.com>
2014-11-26 00:25:10 +0300

    Update float3.c

--- float3.c
+++ float3.c
@@ -3,43 +3,44 @@
 #include <stdio.h>
 int main(void)
 {
-int p, s, e, m, s1, e1, m1;
-union{
- float fval, a, b;
- struct
+  int p, s, e, m, s1, e1, m1;
+  union
   {
- unsigned int m: 23;
- unsigned int e: 8;
- unsigned int s: 1;
-  } bitfields;
-} data;
+    float fval, a, b;
+    struct
+    {
+      unsigned int m: 23;
+      unsigned int e: 8;
+      unsigned int s: 1;
+    } bitfields;
+  } data;
 
-scanf("%f%f", &a, &b);
-data.fval = a / b;
-s1 = data.bitfields.s;
-e1 = data.bitfields.e;
-m1 = data.bitfields.m;
-if (s1 == 0) 
-{
-	s1 = 1;
-}
-else s1 = -1;
-if ((e1 == 255) && (m1 == 0) && (s1 == -1)) 
-{
-	printf("-Infinity\n");
-}
-else if ((e1 == 255) && (m1 == 0) && (s1 == 1)) 
-{
-	printf("+Infinity\n");
-}
-else if ((e1 == 255) && (m1 != 0)) 
-{
-	printf("NaN\n");
-}
-else if ((e1 == 0) && (m1 == 0))
-{
-	printf("NULL\n");
-}
-else printf("%d*2^%d*%f\n", s1, e1-127,1 + ((float)m1 / (1 << 23)));
-return 0;
+  scanf("%f%f", &a, &b);
+  data.fval = a / b;
+  s1 = data.bitfields.s;
+  e1 = data.bitfields.e;
+  m1 = data.bitfields.m;
+  if (s1 == 0) 
+  {
+    s1 = 1;
+  }
+  else (s1 = -1);
+  if ((e1 == 255) && (m1 == 0) && (s1 == -1)) 
+  {
+    printf("-Infinity\n");
+  }
+  else if ((e1 == 255) && (m1 == 0) && (s1 == 1)) 
+  {
+    printf("+Infinity\n");
+  }
+  else if ((e1 == 255) && (m1 != 0)) 
+  {
+    printf("NaN\n");
+  }
+  else if ((e1 == 0) && (m1 == 0))
+  {
+    printf("Zero\n");
+  }
+  else printf("%d*2^%d*%f\n", s1, e1-127,1 + ((float)m1 / (1 << 23)));
+  return 0;
 }
