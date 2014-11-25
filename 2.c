2d32aee099a76bf7833630be2c1de2bb733b0d2f
minonastya <minovskayanastya@gmail.com>
2014-11-26 00:17:21 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -3,39 +3,39 @@
 #include <stdio.h>
 int main(void)
 {
-int n, i;
-float a, b;
-const r = 31;
-scanf("%f""%d", &a, &n);
-if (n == 0) 
-{
-printf("%d\n", 1);
-}
-else 
-{
-  if (n > 0)
+  int n, i;
+  float a, b;
+  const r = 31;
+  scanf("%f""%d", &a, &n);
+  if (n == 0) 
   {
-    b = a; 
-    a = 1;
+    printf("%d\n", 1);
   }
-  else if (n < 0) 
+  else 
   {
-    b = 1/a; 
-    a = 1;
-    n = ~ n + 1;
+    if (n > 0)
+    {
+      b = a; 
+      a = 1;
+    }
+    else if (n < 0) 
+    {
+      b = 1/a; 
+      a = 1;
+      n = ~ n + 1;
+    }
+    for (i = 0; ((n >> r) & 1) == 0; i++)
+    {
+      n = n << 1;
+    }
+    n = n >> i;
+    for (; i <= r; i++) 
+    {
+      if (n & 1 == 1) (a *= b);
+      b *= b;
+      n = n >> 1; 
+    }
+    printf("%f\n", a); 
   }
-  for (i = 0; ((n >> r) & 1) == 0; i++)
-  {
-    n = n << 1;
-  }
-  n = n >> i;
-  for (; i <= r; i++) 
-  {
-    if (n & 1 == 1) a *= b;
-    b *= b;
-    n = n >> 1; 
-  }
-  printf("%f\n", a);
-}
-return 0;
+  return 0;
 }
