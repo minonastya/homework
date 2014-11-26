2a28e2871cc489852f297be7294dc5a54dd7db1f
minonastya <minovskayanastya@gmail.com>
2014-11-26 14:16:04 +0300

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,12 +1,14 @@
-//Влезет ли число n в k битов. 1 - да, 0 - нет.
+//влезет ли число n в k битов
 //Миновская Настя
 #include <stdio.h>
 int main(void)
 {
-int n, k, c;
-const r = 31;
-scanf("%d,%d",&n,&k);
-c = !(((n >> (k - 1)) << (k - 1)) ^ ((n >> r) << (k - 1)));
-printf("%d\n", c);
-return 0;
+  int fitBits(int x, int y)
+  {  
+    return !( (x >> (y - 1)) + !!(x >> (y - 1)));
+  }
+  int n,k;
+  scanf("%d,%d", &n, &k);
+  printf("%d\n", fitBits(n, k));
+  return 0;
 }
