4d04289582f44da5101d0682e9730145d35e5c77
minonastya <minovskayanastya@gmail.com>
2014-12-16 18:21:19 +0300

    Update str.c

--- str.c
+++ str.c
@@ -1,6 +1,5 @@
 //Количество вхождений строки в подстроку
 //Миновская Настя
-
 #include <stdio.h>
 #include <string.h>
 
@@ -9,8 +8,8 @@ int strFound (char str[], char substr[])
   int i, j, k, count = 0;
   for (i = 0; str[i] != '\0'; i++ )
   {
-  for (j = i, k = 0; (substr[k] != '\0') && (str[j] == substr[k]); j++, k++);
-  if ((k > 0) && (substr[k+1] == '\0')) (count++);
+    for (j = i, k = 0; substr[k] != '\0' && str[j] == substr[k]; j++, k++);
+    if ((k > 0) && (substr[k+1] == '\0' || substr[k] == '\0')) (count++);
   }
   return count;
 }
@@ -21,6 +20,7 @@ main()
   char substr[1000];
   fgets(str, 1000 , stdin);
   fgets(substr, 1000 , stdin);
-  printf ("%d\n", strFound(str, substr));
+  if ((strlen(substr) == 1) && (strlen(str) != 1)) printf("%d\n", 0);
+  else printf ("%d\n", strFound(str, substr));
   return 0;
 }
